7641520faf6db0a89b62be0d7943cfced142204d
mankaa <mankaache10@gmail.com>
2022-08-04 21:55:57 +0100

    the project section

--- project.js
+++ project.js
@@ -9,6 +9,7 @@ productContainers.forEach((item, i) => {
     nxtBtn[i].addEventListener('click', () => {
         item.scrollLeft += containerWidth;
     })
+    
 
     preBtn[i].addEventListener('click', () => {
         item.scrollLeft -= containerWidth;
